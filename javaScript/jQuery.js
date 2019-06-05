517614719b2323e89ecd9a7b760b597ce31f8636
poudyalanil <42458094+poudyalanil@users.noreply.github.com>
2019-06-05 10:40:02 +0545

    Add files via upload

---
+++ javaScript/jQuery.js
@@ -0,0 +1,40 @@
+console.log("javaScript is deployed");
+// this JQuery is for articles
+$(".post").hide();
+
+//    <Gautam Buddha>
+
+$("#GB").click(function() {
+  $(".page").hide(1000);
+  $("#Gautama").show(1000);
+});
+
+
+$("#MA").click(function() {
+  $(".page").hide(1000);
+  $("#Manmohan").show(1000);
+});
+
+
+$("#BBA").click(function() {
+  $(".page").hide(1000);
+  $("#Bhanubhakta").show(1000);
+});
+
+
+$("#PLS").click(function() {
+  $(".page").hide(1000);
+  $("#Pasang").show(1000);
+});
+
+
+$("#GPK").click(function() {
+  $(".page").hide(1000);
+  $("#Girija").show(1000);
+});
+
+
+$("#KPB").click(function() {
+  $(".page").hide(1000);
+  $("#Krishna").show(1000);
+});
